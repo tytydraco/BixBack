efe5d26057d1d1071c41db56eb6b841f77967fd8
tytydraco <tylernij@gmail.com>
2018-03-13 22:40:44 -0700

    Bixback mapped to no longer need bixby packages

--- app/src/main/java/com/draco/bixback/AccessibilityService.kt
+++ app/src/main/java/com/draco/bixback/AccessibilityService.kt
@@ -1,6 +1,9 @@
 package com.draco.bixback
 
 import android.accessibilityservice.AccessibilityService
+import android.content.Context
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.os.AsyncTask
 import android.os.Handler
 import android.util.Log
@@ -13,65 +16,90 @@ import java.util.*
 
 class AccessibilityService : AccessibilityService() {
 
-    //val bixPress = "BixbyEventHandler: :Ph::task :: handleKeyEvent :: isConsumed false"
-    val bixPress = "UITask  : :Ph::delivery KeyEvent:ACTION_DOWN"
-    //val logcatCommand = "logcat -s BixbyEventHandler -d -t 100"
-    val logcatCommand = "logcat -s UITask -d -t 100"
+    private val logcatCommand = "logcat -s WindowManager -d -t 10"
+    private val bixLogKeyword = "interceptKeyTi s_result = -1"
+    private val bixPostLogKeyword = "interceptKeyTq s_result=1"
+    private val volumeExclude = "interceptKeyTq s_result=8"
+    private val screenshotExclude = "Destroying surface"
+    private val screenshotExclude2 = "Relative Window"
+    private val screenshotExclude3 = "finishDrawingLocked"
 
-    private val minSpace = 500
-    private val timerMs: Long = 100
-    private var lastOccurrence: Long = 0
+    private val minSpace = 300 // minimum time before next press can be registered
+    private val timerMs: Long = 100 // how many ms to scan for the most recent logs
+    private var lastOccurrence: Long = 0 // keep track of the time since the last press occurred (for minSpace)
 
-    override fun onAccessibilityEvent(event: AccessibilityEvent) {
-        getBixbyLogs()
+    private var torchState = false
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent) { getBixbyLogs() }
+    override fun onServiceConnected() { getBixbyLogs() }
+    override fun onInterrupt() {}
+
+    private fun bixPressFun() {
+        val prefs = getSharedPreferences("bixBackPrefs", Context.MODE_PRIVATE)
+        val action = prefs.getString("action", "back")
+
+        if (action == "flash") {
+            toggleTorch()
+        } else {
+            GlobalTask(this, action).execute()
+        }
     }
 
-    override fun onServiceConnected() {
-        Log.v(TAG, "onServiceConnected")
-        getBixbyLogs()
-    }
-
-
-    override fun onInterrupt() {
-        Log.v(TAG, "onInterrupt")
+    private fun toggleTorch() {
+        val mCameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        if (mCameraManager.getCameraCharacteristics(mCameraManager.cameraIdList[0]).get(CameraCharacteristics.FLASH_INFO_AVAILABLE)) {
+            if (torchState) {
+                mCameraManager.setTorchMode(mCameraManager.cameraIdList[0], false)
+            } else {
+                mCameraManager.setTorchMode(mCameraManager.cameraIdList[0], true)
+            }
+            torchState = !torchState
+        }
     }
 
     private fun getBixbyLogs() {
-        try {
-            val x = run(logcatCommand)
-            if (Calendar.getInstance().timeInMillis - lastOccurrence >= minSpace) {
-                if (x.contains(bixPress)) {
-                    bixPressFun()
-                    lastOccurrence = Calendar.getInstance().timeInMillis
-                }
+        // Make sure keypress doesn't happen too soon
+        if (Calendar.getInstance().timeInMillis - lastOccurrence >= minSpace) {
+            val bixbyLogCommand = run(logcatCommand)
+            if (bixbyLogCommand.contains(bixLogKeyword) && bixbyLogCommand.contains(bixPostLogKeyword) &&
+                    !bixbyLogCommand.contains(volumeExclude) &&
+                    !bixbyLogCommand.contains(screenshotExclude) &&
+                    !bixbyLogCommand.contains(screenshotExclude2) &&
+                    !bixbyLogCommand.contains(screenshotExclude3)) {
+                bixPressFun()
+                lastOccurrence = Calendar.getInstance().timeInMillis
             }
-            val handler = Handler()
-            handler.postDelayed( {
-                getBixbyLogs()
-            }, timerMs)
-        } catch (e: IOException) {
-            e.printStackTrace()
         }
+
+        // Repeat scan indefinitely
+        val handler = Handler()
+        handler.postDelayed( {
+            getBixbyLogs()
+        }, timerMs)
     }
 
-    private fun bixPressFun() {
-        GlobalTask(this).execute()
-        //Log.d("ButtonPressed", "The Bixby Button was pressed")
-    }
-
-    class GlobalTask internal constructor(context: AccessibilityService) : AsyncTask<Void, Void, Void>() {
+    class GlobalTask internal constructor(context: AccessibilityService, action: String) : AsyncTask<Void, Void, Void>() {
         private val activityReference: WeakReference<AccessibilityService> = WeakReference(context)
+        private val orAction = action
+
         override fun doInBackground(vararg voids: Void): Void? {
             try {
                 Thread.sleep(50)
-            } catch (e: InterruptedException) { }
-            activityReference.get()?.performGlobalAction(GLOBAL_ACTION_BACK)
+            } catch (e: InterruptedException) {}
+
+            var globalAction: Int = GLOBAL_ACTION_BACK
+            when (orAction) {
+                "back" -> globalAction = GLOBAL_ACTION_BACK
+                "home" -> globalAction = GLOBAL_ACTION_HOME
+                "recent" -> globalAction = GLOBAL_ACTION_RECENTS
+                "notifications" -> globalAction = GLOBAL_ACTION_NOTIFICATIONS
+                "quick_settings" -> globalAction = GLOBAL_ACTION_QUICK_SETTINGS
+                "power_dialog" -> globalAction = GLOBAL_ACTION_POWER_DIALOG
+                "toggle_split_screen" -> globalAction = GLOBAL_ACTION_TOGGLE_SPLIT_SCREEN
+            }
+            activityReference.get()?.performGlobalAction(globalAction)
             return null
         }
-    }
-
-    companion object {
-        private val TAG = AccessibilityService::class.java.simpleName
     }
 
     private fun run(command: String): String {
---
+++ app/src/main/java/com/draco/bixback/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.draco.bixback
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS
+import android.content.Intent
+import android.provider.Settings
+
+
+/**
+ * Created by Draco on 2/16/2018.
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var back: RadioButton
+    lateinit var home: RadioButton
+    lateinit var recent: RadioButton
+    lateinit var notifications: RadioButton
+    lateinit var quick_settings: RadioButton
+    lateinit var power_dialog: RadioButton
+    lateinit var toggle_split_screen: RadioButton
+    lateinit var flash: RadioButton
+    lateinit var open_accessibility: Button
+
+    lateinit var prefs: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        open_accessibility = findViewById(R.id.open_accessibility)
+
+        back = findViewById(R.id.back)
+        home = findViewById(R.id.home)
+        recent = findViewById(R.id.recent)
+        notifications = findViewById(R.id.notifications)
+        quick_settings = findViewById(R.id.quick_settings)
+        power_dialog = findViewById(R.id.power_dialog)
+        toggle_split_screen = findViewById(R.id.toggle_split_screen)
+        flash = findViewById(R.id.flash)
+
+        prefs = getSharedPreferences("bixBackPrefs", Context.MODE_PRIVATE)
+        editor = prefs.edit()
+
+        val currentAction = prefs.getString("action", "back")
+
+        when (currentAction) {
+            "back" -> back.isChecked = true
+            "home" -> home.isChecked = true
+            "recent" -> recent.isChecked = true
+            "notifications" -> notifications.isChecked = true
+            "quick_settings" -> quick_settings.isChecked = true
+            "power_dialog" -> power_dialog.isChecked = true
+            "toggle_split_screen" -> toggle_split_screen.isChecked = true
+            "flash" -> flash.isChecked = true
+        }
+
+        open_accessibility.setOnClickListener {
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            startActivity(intent)
+        }
+
+        back.setOnClickListener { radioButtonPressed(it) }
+        home.setOnClickListener { radioButtonPressed(it) }
+        recent.setOnClickListener { radioButtonPressed(it) }
+        notifications.setOnClickListener { radioButtonPressed(it) }
+        quick_settings.setOnClickListener { radioButtonPressed(it) }
+        power_dialog.setOnClickListener { radioButtonPressed(it) }
+        toggle_split_screen.setOnClickListener { radioButtonPressed(it) }
+        flash.setOnClickListener { radioButtonPressed(it) }
+    }
+
+    fun radioButtonPressed(view: View) {
+        when (view.id) {
+            R.id.back -> editor.putString("action", "back")
+            R.id.home -> editor.putString("action", "home")
+            R.id.recent -> editor.putString("action", "recent")
+            R.id.notifications -> editor.putString("action", "notifications")
+            R.id.quick_settings -> editor.putString("action", "quick_settings")
+            R.id.power_dialog -> editor.putString("action", "power_dialog")
+            R.id.toggle_split_screen -> editor.putString("action", "toggle_split_screen")
+            R.id.flash -> editor.putString("action", "flash")
+        }
+        editor.apply()
+    }
+}
